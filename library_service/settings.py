c3b2b7ff2c907f111d34cac412acd48373c05ba6
rusip <rusipbox@gmail.com>
2024-06-05 20:10:58 +0300

    first launch with basic functionality (tracked db changes)

--- library_service/settings.py
+++ library_service/settings.py
@@ -148,7 +148,10 @@ AUTH_USER_MODEL = "user.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
 }
 
 SIMPLE_JWT = {
