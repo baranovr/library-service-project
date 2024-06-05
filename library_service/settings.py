e3ae67fb81da5f8c6495ff54cbb507d47f97fb5a
rusip <rusipbox@gmail.com>
2024-06-05 19:09:19 +0300

    modified requirements and settings

--- library_service/settings.py
+++ library_service/settings.py
@@ -27,6 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+# Payment secret key
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
