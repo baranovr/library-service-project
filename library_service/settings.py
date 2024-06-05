f890b227f3a2d83e1524f209e24549a44af462e1
rusip <rusipbox@gmail.com>
2024-06-05 19:36:51 +0300

    implemented Simple JWT

--- library_service/settings.py
+++ library_service/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "rest_framework",
+    "rest_framework_simplejwt",
     "library",
     "user"
 ]
@@ -142,3 +143,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
