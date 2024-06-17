1958292bc8f0531ee9f69850e1ac1be98a63e327
rusip <rusipbox@gmail.com>
2024-06-17 07:40:37 +0300

    modified docker-compose.yaml (fixed issues with urls in settings.py)

--- library_service/settings.py
+++ library_service/settings.py
@@ -190,8 +190,8 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
 REDIS_URL = "redis://localhost:6379/0"
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 CELERY_IMPORTS = ["library.tasks"]
 
